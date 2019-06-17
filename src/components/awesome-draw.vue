1a096ee9a333eb704cb1eea84e5a46fe1443c899
benjaminfff <990460889@qq.com>
2019-06-17 16:40:52 +0800

    awesome-draw abstract tools, pen tool finished.

--- src/components/awesome-draw.vue
+++ src/components/awesome-draw.vue
@@ -37,7 +37,9 @@
         mTransformer: null,
         isTransforming: false,
         isDragging: false,
-        canDraw: true
+        canDraw: true,
+        shiftDown: false,
+        penTool: null
       }
     },
     mounted() {
@@ -47,9 +49,9 @@
       init() {
         let tools = [
           {name: 'pen', isActive: false, type: 'Pen'},
-          {name: 'line', isActive: true, type: 'Shape'},
-          {name: 'elliptical', isActive: false, type: 'Shape'},
-          {name: 'rectangle', isActive: false, type: 'Shape'}
+          {name: 'circle', isActive: false, type: 'Shape'},
+          {name: 'rectangle', isActive: false, type: 'Shape'},
+          {name: 'line', isActive: true, type: 'Shape'}
         ];
         this.tools = tools;
         let mCanvas = document.getElementsByClassName('m-canvas')[0];
@@ -59,16 +61,33 @@
           height: mCanvas.clientHeight
         });
         this.curLayer = new Konva.Layer();
-        this.mTransformer = new Konva.Transformer({});
+        this.mTransformer = new Konva.Transformer({
+          keepRatio: false
+        });
         //this.curLayer.hitGraphEnabled(false);
         this.stage.add(this.curLayer);
         this.curLayer.add(this.mTransformer);
+        this.penTool = {
+          isDrawing: false,
+          lastX: -1,
+          lastY: -1,
+          points: [],
+        }
+        document.addEventListener('keydown', (event) => {
+          event.key == 'Shift' ? this.shiftDown = true : null;
+        });
+        document.addEventListener('keyup', (event) => {
+          event.key == 'Shift' ? this.shiftDown = false : null;
+        });
       },
       dealMousedown(event) {
         let curTool = this.tools.find((tool) => tool.isActive);
         switch (curTool.type) {
           case 'Shape':
             this.dealShapeMouseDown(event);
+            break;
+          case 'Pen':
+            this.dealPenMouseDown(event);
             break;
           default:
             break;
@@ -80,6 +99,9 @@
           case 'Shape':
             this.dealShapeMouseMove(event);
             break;
+          case 'Pen':
+            this.dealPenMouseMove(event);
+            break;
           default:
             break;
         }
@@ -89,6 +111,9 @@
         switch (curTool.type) {
           case 'Shape':
             this.dealShapeMouseUp(event);
+            break;
+          case 'Pen':
+            this.dealPenMouseUp(event);
             break;
           default:
             break;
@@ -100,23 +125,80 @@
       },
       drawLine(x1, y1, x2, y2) {
         let mLine = new Konva.Line({
-          points: [x1, x2, y1, y2],
+          points: [x1, y1, x2, y2],
           stroke: this.color,
           strokeWidth: this.strokeWidth,
         });
         return mLine;
       },
       drawRect(x1, y1, x2, y2) {
+        let shiftDown = this.shiftDown;
         let mRect = new Konva.Rect({
-          x: x1 > x2 ? x1 : x2,
-          y: y1 > y2 ? y1 : y2,
-          width: Math.abs(x1 - x2),
-          height: Math.abs(y1 - y2),
+          x: x1,
+          y: y1,
+          width: x2 - x1,
+          height: !shiftDown ? y2 - y1 : x2 - x1,
           fill: 'transparent',
           stroke: this.color,
           strokeWidth: this.strokeWidth
         });
         return mRect;
+      },
+      drawCircle(x1, y1, x2, y2) {
+        let shiftDown = this.shiftDown;
+        let mRadiusX = Math.abs(x1 - x2);
+        let mRadiusY = Math.abs(y1 - y2);
+        let mOval = new Konva.Ellipse({
+          x: x1,
+          y: y1,
+          radiusX: mRadiusX,
+          radiusY: !shiftDown ? mRadiusY : mRadiusX,
+          fill: 'transparent',
+          stroke: this.color,
+          strokeWidth: this.strokeWidth
+        });
+        return mOval;
+      },
+      dealPenMouseDown(event) {
+        this.penTool.lastX = event.offsetX;
+        this.penTool.lastY = event.offsetY;
+        this.penTool.points = [];
+        this.penTool.points.push(event.offsetX, event.offsetY);
+        this.penTool.isDrawing = true;
+      },
+      dealPenMouseMove(event) {
+        if (!this.penTool.isDrawing) {
+          return;
+        }
+        this.penTool.points.push(event.offsetX, event.offsetY);
+        let points = this.penTool.points;
+        let mLine = new Konva.Line({
+          points: points,
+          stroke: this.color,
+          strokeWidth: this.strokeWidth,
+          lineCap: 'round',
+          lineJoin: 'round',
+          tension: 1
+        });
+        this.curLayer.add(mLine);
+        this.curLayer.draw();
+        mLine.destroy();
+        console.log(points.length);
+      },
+      dealPenMouseUp(event) {
+        let points = this.penTool.points;
+        let mLine = new Konva.Line({
+          points: points,
+          stroke: this.color,
+          strokeWidth: this.strokeWidth,
+          lineCap: 'round',
+          lineJoin: 'round',
+          bezier: true
+        });
+        this.curLayer.add(mLine);
+        this.curLayer.draw();
+        this.penTool.isDrawing = false;
+        console.log(this.curLayer.getChildren().length);
       },
       dealShapeMouseDown(event) {
         if (!this.canDraw) {
@@ -125,7 +207,6 @@
         if (this.lastShape != null) {
           this.lastShape.draggable(false);
         }
-        this.mTransformer.detach();
         this.coords.downX = event.offsetX;
         this.coords.downY = event.offsetY;
         this.drawing = true;
@@ -137,20 +218,25 @@
         this.coords.moveX = event.offsetX;
         this.coords.moveY = event.offsetY;
         let curTool = this.tools.find((tool) => tool.isActive);
-        let mShape;
-        switch (curTool.name) {
-          case 'line':
-            mShape = this.drawLine(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
-            break;
-          case 'rectangle':
-            mShape = this.drawRect(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
-            break;
-          default:
-            break;
+        if (Math.abs(this.coords.moveX - this.coords.downX) > 5 || Math.abs(this.coords.moveY - this.coords.downY) > 5) {
+          let mShape;
+          switch (curTool.name) {
+            case 'line':
+              mShape = this.drawLine(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
+              break;
+            case 'rectangle':
+              mShape = this.drawRect(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
+              break;
+            case 'circle':
+              mShape = this.drawCircle(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
+              break;
+            default:
+              break;
+          }
+          this.curLayer.add(mShape);
+          this.curLayer.draw();
+          mShape.destroy();
         }
-        this.curLayer.add(mShape);
-        this.curLayer.draw();
-        this.curLayer.destroy();
       },
       dealShapeMouseUp(event) {
         this.canDraw = true;
@@ -159,15 +245,18 @@
         }
         this.coords.upX = event.offsetX;
         this.coords.upY = event.offsetY;
-        if (Math.abs(this.coords.upX - this.coords.downX) > 5) {
+        if (Math.abs(this.coords.upX - this.coords.downX) > 5 || Math.abs(this.coords.upY - this.coords.downY) > 5) {
           let curTool = this.tools.find((tool) => tool.isActive);
           let mShape;
           switch (curTool.name) {
             case 'line':
-              mShape = this.drawLine(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
+              mShape = this.drawLine(this.coords.downX, this.coords.downY, this.coords.upX, this.coords.upY);
               break;
             case 'rectangle':
               mShape = this.drawRect(this.coords.downX, this.coords.downY, this.coords.moveX, this.coords.moveY);
+              break;
+            case 'circle':
+              mShape = this.drawCircle(this.coords.downX, this.coords.downY, this.coords.upX, this.coords.upY);
               break;
             default:
               break;
@@ -183,6 +272,16 @@
             this.canDraw = false;
           });
           this.lastShape = mShape;
+        } else {
+          if (this.lastShape != null) {
+            let clonedNode = this.lastShape.clone({
+              x: -1000000
+            });
+            this.curLayer.add(clonedNode);
+            this.mTransformer.attachTo(clonedNode);
+            this.curLayer.draw();
+            clonedNode.destroy();
+          }
         }
         this.drawing = false;
       }
